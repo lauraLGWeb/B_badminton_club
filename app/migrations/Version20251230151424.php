72a28e1808d658248ad2d1f6af5f971bb46961c8
Laura Le Gall <laura.maglegall@gmail.com>
2025-12-30 16:39:34 +0100

    entity product created with fixture

---
+++ app/migrations/Version20251230151424.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251230151424 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, price INT NOT NULL, size VARCHAR(5) NOT NULL, picture VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user CHANGE lience_nbr lience_nbr BIGINT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE product');
+        $this->addSql('ALTER TABLE user CHANGE lience_nbr lience_nbr INT NOT NULL');
+    }
+}
---
+++ app/src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+
+class ProductFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        // T-shirt
+        $tshirt = new Product();
+        $tshirt->setTitle('T-shirt basique')
+               ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+               ->setPrice(40)
+               ->setSize('') // size will be chosen by the purchaser later
+               ->setPicture('tshirt.jpg');
+        $manager->persist($tshirt);
+
+        // Boîte de volants
+        $shuttlecocks = new Product();
+        $shuttlecocks->setTitle('Boîte de volants')
+                      ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+                      ->setPrice(30)
+                      ->setSize('') // no size
+                      ->setPicture('shuttlecocks.jpg');
+        $manager->persist($shuttlecocks);
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['products'];
+    }
+}
