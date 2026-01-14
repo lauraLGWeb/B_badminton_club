3254a570b5990261208874a42685deb49cbfdc2a
Laura Le Gall <laura.maglegall@gmail.com>
2026-01-14 17:42:45 +0100

    resolving maj when modifying + twig on inscription and prices page + hearder for leclub

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -21,38 +21,33 @@ final class HomeController extends AbstractController
         ]);
    }
 
-// routes for the club dropdown
-    #[Route('/Leclub/Membres', name: 'app_members')]
-    public function members(): Response
-    {
-        return $this->render('home/CaMember.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
 
+
+
+// routes for the club dropdown
+  
       #[Route('/Leclub/Entraineurs', name: 'app_coaches')]
     public function coaches(): Response
     {
-        return $this->render('home/coaches.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/coaches.html.twig');
     }
 
+    #[Route('/Leclub/Membres', name: 'app_caMembers')]
+    public function caMembers(): Response
+    {
+        return $this->render('home/caMembers.html.twig');
+    }
 
 #[Route('/Leclub/Reglement', name: 'app_rules')]
     public function rules(): Response
     {
-        return $this->render('home/rules.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/rules.html.twig');
     }
 
         #[Route('/Leclub/Stages', name: 'app_internships')]
     public function internships(): Response
     {
-        return $this->render('home/internships.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/internships.html.twig');
     }
 
 
@@ -66,13 +61,7 @@ final class HomeController extends AbstractController
      
   
 // routes for the ecole de bad dropdown
-    // #[Route('/Leclub/Membres', name: 'app_members')]
-    // public function members(): Response
-    // {
-    //     return $this->render('home/CaMember.html.twig', [
-    //         'controller_name' => 'HomeController',
-    //     ]);
-    // }
+ 
 
     //   #[Route('/Leclub/Entraineurs', name: 'app_coaches')]
     // public function coaches(): Response
