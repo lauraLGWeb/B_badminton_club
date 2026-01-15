4854deb48f1360290e171af291ad276d9d93c4e4
Laura Le Gall <laura.maglegall@gmail.com>
2026-01-15 18:00:51 +0100

    test for CartItems done

--- app/tests/unit/Entity/CartEntityTest.php
+++ app/tests/unit/Entity/CartEntityTest.php
@@ -9,6 +9,8 @@ use PHPUnit\Framework\TestCase;
 
 class CartEntityTest extends TestCase
 {
+
+    // get the cart
     public function testCartInitialization(): void
     {
         $cart = new Cart();
@@ -19,6 +21,8 @@ class CartEntityTest extends TestCase
         $this->assertCount(0, $cart->getCartItem());
     }
 
+
+    // is the cart paid ?
     public function testIsPaid(): void
     {
         $cart = new Cart();
@@ -27,6 +31,8 @@ class CartEntityTest extends TestCase
         $this->assertTrue($cart->isPaid());
     }
 
+
+    //set up the purschase date
     public function testPurchaseDate(): void
     {
         $cart = new Cart();
@@ -37,6 +43,7 @@ class CartEntityTest extends TestCase
         $this->assertSame($date, $cart->getPurchaseDate());
     }
 
+    // create a cart to a user 
     public function testUserRelation(): void
     {
         $cart = new Cart();
@@ -47,6 +54,8 @@ class CartEntityTest extends TestCase
         $this->assertSame($user, $cart->getUser());
     }
 
+
+    // add an item to the cart 
     public function testAddCartItem(): void
     {
         $cart = new Cart();
@@ -58,6 +67,8 @@ class CartEntityTest extends TestCase
         $this->assertSame($cart, $cartItem->getCart());
     }
 
+
+     // delete an item to the cart 
     public function testRemoveCartItem(): void
     {
         $cart = new Cart();
